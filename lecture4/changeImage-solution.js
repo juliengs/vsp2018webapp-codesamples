4f322ded67ced8a8ee655105c52c44a4f3965c9f
karthikp-ubc <karthikp@ece.ubc.ca>
2018-07-25 08:23:46 -0700

    Made the change in image order more explicit

---
+++ lecture4/changeImage-solution.js
@@ -0,0 +1,27 @@
+var changeImages = function(id,  offset) {
+	var div = document.getElementById(id);
+	var imgtags = div.getElementsByTagName("img");
+
+	function changeImage() {
+		if (imgtags.length==0) return;
+		var first = imgtags[0].src;
+		for (var i=0; i<imgtags.length; i++) {
+			var img = imgtags[i];
+			var nextIndex = (i + 1);
+			console.log("Replacing " + i + " with " + nextIndex); 
+			if (nextIndex==imgtags.length) 
+				img.src = first;
+			else
+				img.src = imgtags[nextIndex].src;
+		}
+	}
+	
+	for (var k=0; k<offset; k++)
+		changeImage();
+}
+
+window.onload = function() {
+	setInterval(function() { 
+		changeImages("parent", 1);
+	}, 5000);
+};
